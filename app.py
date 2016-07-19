301cd5985ec1ae9cbb450b4fbf6e202e1e44706a
yuya yano <yuya.yano.6260@gmail.com>
2016-07-19 09:52:02 +0900

    Avoid 404 Not Found on Flask.

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def index(path):
     return render_template('index.html')
 
 if __name__ == '__main__':
